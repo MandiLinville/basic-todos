f649197efaf1219ed369957833d0d7e5c4f0eb8b
runnerguy55 <michaelhoffmanj@gmail.com>
2017-02-01 10:25:11 -0700

    solutions!

--- todos.js
+++ todos.js
@@ -1,13 +1,25 @@
 const vm = new Vue({
     el: '#root',
-    data() {
-        return {
-            todos: []
-        }
+    data: {
+        todos: [],
+        todo: ''
+        
     },
     methods: {
         submitTodo(todo) {
-            this.todos.push(todo)
+            this.todos.push({
+                task: todo,
+                completed: false
+            })
+            this.todo='' 
+        },
+        removeItem(todo){
+        var i = this.todos.indexOf(todo)
+            this.todos.splice(i,1)
+        },
+        completedTodo(todo){
+            debugger 
+            todo.completed=!todo.completed
         }
     }
 })
